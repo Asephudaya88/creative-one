f93d6fd9f68d373256fc29fddd625b809f05f0ea
Asep Saepul Hudaya <asepsaepulhudaya@gmail.com>
2026-07-03 06:47:10 +0700

    Sprint 4: Modular Architecture Foundation

---
+++ src/features/dashboard/index.tsx
@@ -0,0 +1,8 @@
+export default function Dashboard() {
+  return (
+    <div>
+      <h1>Creative One Dashboard</h1>
+      <p>Sprint 4 Enterprise Architecture</p>
+    </div>
+  );
+}
---
+++ src/pages/DashboardPage.tsx
@@ -0,0 +1,24 @@
+export default function DashboardPage() {
+  return (
+    <div style={{ padding: "30px" }}>
+      <h1>🚀 Creative One Dashboard</h1>
+
+      <p>
+        Selamat datang di <b>Creative One</b>
+      </p>
+
+      <hr />
+
+      <h3>Modul yang sedang dikembangkan</h3>
+
+      <ul>
+        <li>🏘 Creative Village</li>
+        <li>💧 Creative Water</li>
+        <li>🏫 Creative School</li>
+        <li>🕌 Creative Foundation</li>
+        <li>🎨 Creative Studio</li>
+        <li>⚖ Creative Faraidh</li>
+      </ul>
+    </div>
+  );
+}
